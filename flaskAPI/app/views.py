62b8b24ff53b5ab8ebee3b88b06438d5983cf46b
Alex McAfee <alexmcafee@outlook.com>
2016-09-12 08:19:31 -0700

    Initial Commit
    Added discitonaries, and Flask Framework.
    +initial functionality on localhost on generating random string in form
    of adjective+adverb+noun

---
+++ flaskAPI/app/views.py
@@ -0,0 +1,26 @@
+from app import app
+from random import randint
+
+adjectives, adverbs, nouns = [], [], []
+with open("C:\\Users\\amcafee\\Documents\\GitHub\\shortener\\adjectives\\adjectives.txt") as f:
+	adjectives = f.read().split()
+with open("C:\\Users\\amcafee\\Documents\\GitHub\\shortener\\adverbs\\adverbs.txt") as f:
+	adverbs = f.read().split()
+with open("C:\\Users\\amcafee\\Documents\\GitHub\\shortener\\nouns\\nouns.txt") as f:
+	nouns = f.read().split()
+
+print((adjectives[0]  + adjectives[1]).encode("utf-8", errors='waaaat'))
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return "Hello, World. Fucker"
+
+@app.route('/shortener')
+def shortener():
+	return (adjectives[randint(0,len(adjectives))] + adverbs[randint(0,len(adverbs))] + nouns[randint(0,len(nouns))])
+
+@app.route('/test')
+def tester():
+	return "Testing"
+
